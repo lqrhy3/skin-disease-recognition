ab289276cb2106abb8e68e176e65e12926c1253a
lqrhy3 <m1xst99@yandex.ru>
2023-02-19 15:08:08 +0300

    reformat data

---
+++ src/data/convert_acne04_to_voc.py
@@ -0,0 +1,60 @@
+import os.path
+import shutil
+import subprocess
+import sys
+
+import click
+
+BAKED_DATA_NAME = 'acne04_voc'
+
+
+@click.command()
+@click.option('--raw_data_dir', type=str, help='Path to a folder with raw Acne04 data.')
+@click.option('--baked_data_dir', type=str, help='Path to a directory where to save prepared data'
+                                                 ' (`acne04_voc` folder will be created there).')
+def main(raw_data_dir: str, baked_data_dir: str):
+    baked_data_dir = os.path.join(baked_data_dir, BAKED_DATA_NAME)
+    unzip_archives(raw_data_dir)
+    create_baked_data_folders(baked_data_dir)
+    move_images(raw_data_dir, baked_data_dir)
+    move_annotations(raw_data_dir, baked_data_dir)
+
+
+def unzip_archives(raw_data_root: str):
+    assert 'win' not in sys.platform, 'ochen\' zhal\'.'
+
+    if not os.path.exists(os.path.join(raw_data_root, 'Classification')):
+        path_to_zip = os.path.join(raw_data_root, 'Classification.tar')
+        assert os.path.exists(path_to_zip)
+
+        cmd = ['tar', '-xf', path_to_zip, '-C', raw_data_root]
+        subprocess.run(cmd)
+
+    if not os.path.exists(os.path.join(raw_data_root, 'Detection')):
+        path_to_zip = os.path.join(raw_data_root, 'Detection.tar')
+        assert os.path.exists(path_to_zip)
+
+        cmd = ['tar', '-xf', path_to_zip, '-C', raw_data_root]
+        subprocess.run(cmd)
+
+
+def create_baked_data_folders(baked_data_dir: str):
+    os.makedirs(baked_data_dir, exist_ok=True)
+    os.makedirs(os.path.join(baked_data_dir, 'data'), exist_ok=True)
+    os.makedirs(os.path.join(baked_data_dir, 'labels'), exist_ok=True)
+
+
+def move_images(raw_data_dir: str, baked_data_dir: str):
+    path_to_images_dir = os.path.join(raw_data_dir, 'Classification', 'JPEGImages')
+    path_to_baked_images_dir = os.path.join(baked_data_dir, 'data')
+    shutil.copytree(path_to_images_dir, path_to_baked_images_dir, dirs_exist_ok=True)
+
+
+def move_annotations(raw_data_dir: str, baked_data_dir: str):
+    path_to_anns_dir = os.path.join(raw_data_dir, 'Detection', 'VOC2007', 'Annotations')
+    path_to_baked_anns_dir = os.path.join(baked_data_dir, 'labels')
+    shutil.copytree(path_to_anns_dir, path_to_baked_anns_dir, dirs_exist_ok=True)
+
+
+if __name__ == '__main__':
+    main()
