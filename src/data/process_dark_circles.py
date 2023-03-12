37a4c2a7c4bac24ba52a5acb2530c111efa5c156
lqrhy3 <m1xst99@yandex.ru>
2023-03-13 01:21:14 +0300

    fix

--- src/data/process_dark_circles.py
+++ src/data/process_dark_circles.py
@@ -1,6 +1,7 @@
 import shutil
 from pathlib import Path
 import os
+from typing import Tuple
 
 from tqdm import tqdm
 import click
@@ -27,15 +28,22 @@ def main(raw_data_dir: str, processed_data_dir: str):
     (processed_data_dir / DATA_NAME).mkdir(exist_ok=True)
     (processed_data_dir / LABELS_NAME).mkdir(exist_ok=True)
 
+    num_strange_files = 0
+    dropped_label_names = list()
+
     for filename in tqdm(os.listdir(raw_data_dir / 'dark_circles'), desc='Processing data samples'):
         # processing labels
         filename = Path(filename)
         path_to_label = raw_data_dir / 'dark_circles' / filename
         if not path_to_label.is_file():
+            num_strange_files += 1
             continue
 
         label = cv2.imread(path_to_label.as_posix())
-        processed_label = process_label(label)
+        processed_label, drop_label = process_label(label)
+        if drop_label:
+            dropped_label_names.append(filename.stem)
+            continue
 
         path_to_save_label = processed_data_dir / LABELS_NAME / filename.with_suffix('.PNG')
         cv2.imwrite(path_to_save_label.as_posix(), processed_label)
@@ -45,6 +53,9 @@ def main(raw_data_dir: str, processed_data_dir: str):
         path_to_save_image = processed_data_dir / DATA_NAME / filename.with_suffix('.PNG')
         image = cv2.imread(path_to_raw_image.as_posix())
         cv2.imwrite(path_to_save_image.as_posix(), image)
+
+    print(f'Number of dropped samples: {len(dropped_label_names)}.')
+    print(f'Dropped sample names: {dropped_label_names}.')
 
 
 def clean_processed_dir_if_needed(processed_data_dir: os.PathLike):
@@ -56,10 +67,13 @@ def clean_processed_dir_if_needed(processed_data_dir: os.PathLike):
             shutil.rmtree(processed_data_dir)
 
 
-def process_label(label: np.ndarray) -> np.ndarray:
+def process_label(label: np.ndarray) -> Tuple[np.ndarray, bool]:
     label = binarize_label(label)
+    is_empty = (label == 0).all()
     label = remove_noise_from_label(label)
-    return label
+    is_empty_after_cleaning = (label == 0).all()
+    drop_label = is_empty != is_empty_after_cleaning
+    return label, drop_label
 
 
 def binarize_label(label: np.ndarray) -> np.ndarray:
