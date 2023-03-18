f0cfdbd70a6a7ae4b301854f6d7ee665a8626ebe
lqrhy3 <m1xst99@yandex.ru>
2023-03-19 02:41:11 +0300

    enhance image prediction callback

--- src/callbacks/log_prediction_images.py
+++ src/callbacks/log_prediction_images.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
+from monai.visualize import blend_images
 
 from src.utils.data import unnormalize_imagenet
 
@@ -21,17 +22,32 @@ class LogPredictionImagesCallback(pl.Callback):
         wandb_logger = trainer.logger
         assert isinstance(wandb_logger, WandbLogger)
 
-        preds = outputs['preds'][:self.num_samples].detach()  # [N, 1, H, W]
-        samples = batch[0][:self.num_samples]  # [N, 3, H, W]
-        gts = batch[1][:self.num_samples]  # [N, 1, H, W]
+        masks_pred = outputs['preds'][:self.num_samples].detach()  # [N, 1, H, W]
+        images = batch[0][:self.num_samples]  # [N, 3, H, W]
+        masks_gt = batch[1][:self.num_samples]  # [N, 1, H, W]
 
-        preds = (preds * 255).expand_as(samples)  # [N, 3, H, W]
-        samples = torch.clamp(unnormalize_imagenet(samples) * 255, 0, 255)  # [N, 3, H, W]
-        gts = (gts * 255).expand_as(samples)  # [N, 3, H, W]
+        images = unnormalize_imagenet(images)
 
-        visualizations = torch.cat([preds, samples, gts], dim=-1)  # [N, 3, H, 3*W]
-        visualizations = visualizations.permute(0, 2, 3, 1).cpu().numpy().astype(np.uint8)  # [N, H, 3*W, 3]
-        visualizations = [vis for vis in visualizations]
+        raw_masks_vis = []
+        blend_masks_vis = []
+        for i in range(len(images)):
+            image = images[i]
+            mask_pred = masks_pred[i]
+            mask_gt = masks_gt[i]
+
+            raw_masks_vis.append(
+                torch.cat([mask_pred, mask_gt], dim=-1).permute(1, 2, 0)
+            )
+            blend_pred = blend_images(image, mask_pred, rescale_arrays=False, cmap='hsv')
+            blend_gt = blend_images(image, mask_gt, rescale_arrays=False, cmap='winter')
+            blend_masks_vis.append(
+                torch.cat([blend_pred, blend_gt], dim=-1).permute(1, 2, 0)  # [H, 2*W, 3]
+            )
 
         # ochen' jal', but step passing does not work
-        wandb_logger.log_image(key='prediction_images', images=visualizations)
+        wandb_logger.log_image(key='prediction_images/masks', images=raw_masks_vis)
+        wandb_logger.log_image(
+            key='prediction_images/blend_masks',
+            images=blend_masks_vis,
+            caption=['prediction vs ground_truth'] * len(blend_masks_vis)
+        )
