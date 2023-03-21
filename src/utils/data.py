4c8ea3dbb24c9b0818a5fde77db0c2b24a24b819
JJBT <chernvld@gmail.com>
2023-03-21 02:15:50 +0100

    hotfix in `src/utils/data.py`

--- src/utils/data.py
+++ src/utils/data.py
@@ -6,8 +6,8 @@ IMAGENET_STD = [0.229, 0.224, 0.225]
 
 
 def unnormalize_imagenet(image: torch.Tensor):
-    mean = torch.tensor(IMAGENET_MEAN)  # [3,]
-    std = torch.tensor(IMAGENET_STD)  # [3,]
+    mean = torch.tensor(IMAGENET_MEAN, device=image.device)  # [3,]
+    std = torch.tensor(IMAGENET_STD, device=image.device)  # [3,]
 
     if image.ndim == 4:
         assert image.shape[1] == 3
