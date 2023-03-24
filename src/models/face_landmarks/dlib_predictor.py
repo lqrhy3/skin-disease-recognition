6ccf7f571798556551caa5ae024eec55ba82d701
JJBT <chernvld@gmail.com>
2023-03-24 21:28:38 +0100

    added some fucks

---
+++ src/models/face_landmarks/dlib_predictor.py
@@ -0,0 +1,63 @@
+from copy import deepcopy
+from typing import Tuple
+
+import dlib
+import sys
+import cv2
+import numpy as np
+
+
+LEFT_EYE_IDXS = [36, 37, 38, 39, 40, 41]
+RIGHT_EYE_IDXS = [42, 43, 44, 45, 46, 47]
+
+
+def draw_face_landmarks(image: np.ndarray, landmarks, color: Tuple = (0, 255, 0), radius: int = 3) -> np.ndarray:
+    image = deepcopy(image)
+    for i, p in enumerate(landmarks.parts()):
+        cv2.circle(image, (p.x, p.y), radius, color, -1)
+        cv2.putText(image, str(i), (p.x + 5, p.y + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+
+    return image
+
+
+class LandmarksDlibPredictor:
+    def __init__(self, predictor_path: str):
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(predictor_path)
+
+    def __call__(self, image: np.ndarray):
+        """
+        :poram image: RGB
+        """
+        faces = self.detector(image, 0)
+        face = faces[0]
+        bbox = dlib.rectangle(int(face.left()), int(face.top()), int(face.right()), int(face.bottom()))
+        landmarks = self.predictor(image, bbox)
+        return landmarks
+
+
+def create_eyes_mask(image: np.ndarray, face_landmarks) -> np.ndarray:
+    mask = np.zeros((image.shape[0], image.shape[1]))
+
+    landmarks_list = face_landmarks.parts()
+
+    left_eye_pts = [[landmarks_list[idx].x, landmarks_list[idx].y] for idx in LEFT_EYE_IDXS]
+    right_eye_pts = [[landmarks_list[idx].x, landmarks_list[idx].y] for idx in RIGHT_EYE_IDXS]
+    left_eye_pts = np.array(left_eye_pts)
+    right_eye_pts = np.array(right_eye_pts)
+
+    cv2.fillPoly(mask, [left_eye_pts, right_eye_pts], color=(255, 255, 255))
+    return mask
+
+
+if __name__ == '__main__':
+    path_to_predictor = '/Users/vladimir/dev/data/shape_predictor_68_face_landmarks.dat'
+    landmarks_predictor = LandmarksDlibPredictor(path_to_predictor)
+    path_to_image = '/Users/vladimir/dev/data/dark_circles_train/data/IMG_0626.PNG'
+    image = cv2.imread(path_to_image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    landmarks = landmarks_predictor(image)
+    result_image = draw_face_landmarks(image, landmarks)
+    mask = create_eyes_mask(image, landmarks)
+    masked_image = np.logical_not(mask)[:, :, None] * image
+    pass
--- src/models/noml/noml.py
+++ src/models/noml/noml.py
@@ -1,12 +1,26 @@
 import math
+from copy import deepcopy
+from pathlib import Path
+from typing import Tuple
+
+import numpy as np
+import cv2
+
+from src.models.face_landmarks import (
+    LandmarksDlibPredictor,
+    draw_face_landmarks,
+    create_eyes_mask,
+    LEFT_EYE_IDXS,
+    RIGHT_EYE_IDXS,
+)
 
 
 def find_equidistant_point(x1, y1, x2, y2):
     ax = x2 - x1
     ay = y2 - y1
 
-    nx = 1
-    ny = -ax / ay
+    nx = -ay / ax
+    ny = 1
 
     nx = nx / math.hypot(nx, ny)
     ny = ny / math.hypot(nx, ny)
@@ -42,3 +56,174 @@ def find_circle(x1, y1, x2, y2, x3, y3):
 
     radius = math.sqrt((cx - x1) ** 2 + (cy - y1) ** 2)
     return cx, cy, radius
+
+
+class NoMLClassifier:
+    def __init__(self, landmarks_predictor_path: str, thr: float):
+        self.landmarks_predictor = LandmarksDlibPredictor(landmarks_predictor_path)
+        self.thr = thr
+
+    def __call__(self, image: np.ndarray):
+        face_landmarks = self.landmarks_predictor(image)
+        rule_mask, rure_mask, rarule_mask, rarure_mask = self.calculate_region_masks(image, face_landmarks)
+        rule_intensity = self.calculate_intensity_by_mask(image, rule_mask)
+        rure_intensity = self.calculate_intensity_by_mask(image, rure_mask)
+        rarule_intensity = self.calculate_intensity_by_mask(image, rarule_mask)
+        rarure_intensity = self.calculate_intensity_by_mask(image, rarure_mask)
+        mask = np.logical_or(np.logical_or(rule_mask, rure_mask), np.logical_or(rarule_mask, rarure_mask))
+
+        left_measure = rarule_intensity - rule_intensity
+        right_measure = rarure_intensity - rure_intensity
+        mean_measure = 0.5 * (left_measure + right_measure)
+        predicted_label = mean_measure > self.thr
+
+        # print(f'{left_measure=},\n '
+        #       f'{right_measure=},\n '
+        #       f'{rule_intensity=},\n '
+        #       f'{rarule_intensity=},\n '
+        #       f'{rure_intensity=}, \n'
+        #       f'{rarure_intensity=}\n'
+        #       f'========================')
+
+        return predicted_label, mask
+
+    def calculate_intensity_by_mask(self, image: np.ndarray, mask: np.ndarray) -> float:
+        roi = image * mask[:, :, None]
+        return roi[roi.nonzero()].mean()
+
+    def calculate_region_masks(self, image: np.ndarray, face_landmarks):
+        left_eye_circle_mask, right_eye_circle_mask, left_eye_circle, right_eye_circle = \
+            self.create_circles_mask(image, face_landmarks)
+        left_pillar_mask, right_pillar_mask = self.create_eyes_pillar_masks(image, face_landmarks)
+
+        rule_mask = np.logical_and(left_eye_circle_mask, np.logical_not(left_pillar_mask))
+        rure_mask = np.logical_and(right_eye_circle_mask, np.logical_not(right_pillar_mask))
+
+        rarule_mask, rarure_mask = self.create_rarue_mask(
+            image, left_eye_circle_mask, left_eye_circle, right_eye_circle_mask, right_eye_circle, face_landmarks, 2
+        )
+
+        return rule_mask, rure_mask, rarule_mask, rarure_mask
+
+    def create_rarue_mask(
+            self,
+            image: np.ndarray,
+            left_eye_circle_mask: np.ndarray,
+            left_rue_circle: Tuple,
+            right_eye_circle_mask: np.ndarray,
+            right_rue_circle: Tuple,
+            face_landmarks,
+            radius_factor: float
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        blank_mask = np.zeros((image.shape[0], image.shape[1]))
+        rarule_mask = deepcopy(blank_mask)
+        rarure_mask = deepcopy(blank_mask)
+
+        cv2.circle(rarule_mask, (int(left_rue_circle[0]), int(left_rue_circle[1])), int(radius_factor * left_rue_circle[2]), (255, ), -1)
+        cv2.circle(rarure_mask, (int(right_rue_circle[0]), int(right_rue_circle[1])),  int(radius_factor * right_rue_circle[2]), (255, ), -1)
+
+        rarule_mask = np.logical_and(rarule_mask, np.logical_not(left_eye_circle_mask))
+        rarure_mask = np.logical_and(rarure_mask, np.logical_not(right_eye_circle_mask))
+
+        left_triangle_pts = [[face_landmarks.parts()[idx].x, face_landmarks.parts()[idx].y] for idx in [1, 38, 48]]
+        left_triangle_pts = np.array(left_triangle_pts)
+        left_triangle_mask = cv2.fillPoly(deepcopy(blank_mask), [left_triangle_pts], color=(255, ))
+
+        right_triangle_pts = [[face_landmarks.parts()[idx].x, face_landmarks.parts()[idx].y] for idx in [15, 43, 54]]
+        right_triangle_pts = np.array(right_triangle_pts)
+        right_triangle_mask = cv2.fillPoly(deepcopy(blank_mask), [right_triangle_pts], color=(255, ))
+
+        rarule_mask = np.logical_and(rarule_mask, left_triangle_mask)
+        rarure_mask = np.logical_and(rarure_mask, right_triangle_mask)
+        return rarule_mask, rarure_mask
+
+    def create_eyes_pillar_masks(self, image: np.ndarray, face_landmarks) -> Tuple[np.ndarray, np.ndarray]:
+        left_pillar_mask = np.zeros((image.shape[0], image.shape[1]))
+        right_pillar_mask = np.zeros((image.shape[0], image.shape[1]))
+
+        left_pillar_pts = [[face_landmarks.parts()[idx].x, face_landmarks.parts()[idx].y] for idx in [36, 41, 40, 39]]
+        left_pillar_pts.extend([[face_landmarks.parts()[36].x, 0], [face_landmarks.parts()[39].x, 0]])
+        left_pillar_pts = np.array(left_pillar_pts)
+        cv2.fillPoly(left_pillar_mask, [left_pillar_pts], color=(255, ))
+
+        right_pillar_pts = [[face_landmarks.parts()[idx].x, face_landmarks.parts()[idx].y] for idx in [42, 47, 46, 45]]
+        right_pillar_pts.extend([[face_landmarks.parts()[42].x, 0], [face_landmarks.parts()[45].x, 0]])
+        right_pillar_pts = np.array(right_pillar_pts)
+        cv2.fillPoly(right_pillar_mask, [right_pillar_pts], color=(255, ))
+
+        # TODO: make it relative to eye size
+        kernel = np.ones((7, 7))
+        iterations = 2
+        left_pillar_mask = cv2.dilate(left_pillar_mask, kernel, iterations=iterations)
+        right_pillar_mask = cv2.dilate(right_pillar_mask, kernel, iterations=iterations)
+
+        return left_pillar_mask, right_pillar_mask
+
+    def create_circles_mask(self, image: np.ndarray, face_landmarks):
+        plb = find_equidistant_point(
+            x1=face_landmarks.parts()[36].x,
+            y1=face_landmarks.parts()[36].y,
+            x2=face_landmarks.parts()[39].x,
+            y2=face_landmarks.parts()[39].y,
+        )
+        prb = find_equidistant_point(
+            x1=face_landmarks.parts()[42].x,
+            y1=face_landmarks.parts()[42].y,
+            x2=face_landmarks.parts()[45].x,
+            y2=face_landmarks.parts()[45].y,
+        )
+
+        left_eye_circle = find_circle(
+            x1=face_landmarks.parts()[36].x,
+            y1=face_landmarks.parts()[36].y,
+            x2=face_landmarks.parts()[39].x,
+            y2=face_landmarks.parts()[39].y,
+            x3=plb[0],
+            y3=plb[1],
+        )
+
+        right_eye_circle = find_circle(
+            x1=face_landmarks.parts()[42].x,
+            y1=face_landmarks.parts()[42].y,
+            x2=face_landmarks.parts()[45].x,
+            y2=face_landmarks.parts()[45].y,
+            x3=prb[0],
+            y3=prb[1],
+        )
+
+        eye_circles_mask = np.zeros((image.shape[0], image.shape[1]))
+        left_eye_circle_mask = deepcopy(eye_circles_mask)
+        right_eye_circle_mask = deepcopy(eye_circles_mask)
+
+        cv2.circle(left_eye_circle_mask, (int(left_eye_circle[0]), int(left_eye_circle[1])), int(left_eye_circle[2]), (255,), -1)
+        cv2.circle(right_eye_circle_mask, (int(right_eye_circle[0]), int(right_eye_circle[1])), int(right_eye_circle[2]), (255,), -1)
+
+        return left_eye_circle_mask, right_eye_circle_mask, left_eye_circle, right_eye_circle
+
+
+def test_imgs(path_to_image, path_to_predictor, thr):
+    # visualizer
+    clf = NoMLClassifier(path_to_predictor, thr)
+    image = cv2.imread(path_to_image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result, mask = clf(image)
+    masked_image = np.logical_not(mask)[:, :, None] * image
+    result_image = np.concatenate([masked_image, image], axis=1)
+    result_image = cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR)
+    cv2.imshow('1', result_image)
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+    return masked_image
+
+
+if __name__ == '__main__':
+    path_to_predictor = '/Users/vladimir/dev/data/shape_predictor_68_face_landmarks.dat'
+    path_to_folder = Path('/Users/vladimir/dev/data/dark_circles_train/data')
+    for path_to_image in path_to_folder.glob('*'):
+        print(path_to_image.name)
+        try:
+            test_imgs(path_to_image.as_posix(), path_to_predictor, 0)
+        except:
+            print('!!!!!!!!!')
+
+    pass
